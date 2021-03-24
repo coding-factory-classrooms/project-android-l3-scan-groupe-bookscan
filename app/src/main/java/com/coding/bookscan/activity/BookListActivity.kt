7374e0e4b0f4990514114c20050eb1818de2a7ef
Melanie V <melanie.vaubien@hotmail.com>
2021-03-24 10:57:27 +0100

    File Init

---
+++ app/src/main/java/com/coding/bookscan/activity/BookListActivity.kt
@@ -0,0 +1,12 @@
+package com.coding.bookscan.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.coding.bookscan.R
+
+class BookListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_list)
+    }
+}
---
+++ app/src/main/java/com/coding/bookscan/activity/BookListAdapter.kt
@@ -0,0 +1,4 @@
+package com.coding.bookscan.activity
+
+class BookListAdapter {
+}
